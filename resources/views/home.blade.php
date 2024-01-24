44084744f3f1bb78761de9b76f71229f66b93ed6
chiara.ruggi <chiara.ruggi1998@gmail.com>
2024-01-24 17:46:09 +0100

    added P on home.blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>Dati da config/data.js: {{ $home }}</h1>
-<p>This is my paragraph content.</p>
-<button class="btn btn-primary">Cliccami</button>
+    <h1>Dati da config/data.js: {{ $home }}</h1>
+    <p>This is my paragraph content.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi saepe distinctio esse velit voluptatibus? Error,
+        iure. Incidunt perferendis vitae amet tempore reiciendis nihil placeat, non quisquam. Ex facilis ullam hic.</p>
+    <button class="btn btn-primary">Cliccami</button>
 @endsection
