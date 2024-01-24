98ab111c1c0b906e1a1d39fad3092ad0e309390f
Marco <marcoregina92@gmail.com>
2024-01-24 17:53:59 +0100

    add h2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
     <h1>Dati da config/data.js: {{ $home }}</h1>
+    <h2>New component</h2>
     <p>This is my paragraph content.</p>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi saepe distinctio esse velit voluptatibus? Error,
         iure. Incidunt perferendis vitae amet tempore reiciendis nihil placeat, non quisquam. Ex facilis ullam hic.</p>
